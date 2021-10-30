1e4325598853555041a235b47a63413307665ba8
= <=>
2021-10-29 21:52:28 -0300

    Adicionado Autenticação

---
+++ src/main/java/com/kelvin/os/security/SpringWebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.kelvin.os.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
