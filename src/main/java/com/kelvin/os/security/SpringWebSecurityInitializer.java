e4b7e2aa5c6ad6ee8784c6961a9b830a74b2b64b
= <=>
2021-10-29 22:00:41 -0300

    Revert "Adicionado Autenticação"
    This reverts commit 1e4325598853555041a235b47a63413307665ba8.

--- src/main/java/com/kelvin/os/security/SpringWebSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.kelvin.os.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
